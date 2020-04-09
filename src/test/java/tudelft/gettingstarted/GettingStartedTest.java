4d503ed833a2f4466df0bbd669bfe0b41a248c8e
DinhDerek <derekkd@uci.edu>
2020-04-09 16:27:16 -0700

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -13,15 +13,27 @@ public class GettingStartedTest {
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
+    
+    @Test
+    public void addFiveToMinus45() {
+        int result = new GettingStarted().addFive(-45);
+        Assertions.assertEquals(-40,result);
+    }
+    
+    @Test
+    public void addFiveTo35() {
+        int result = new GettingStarted().addFive(35);
+        Assertions.assertEquals(40,result);
+    }
 }
